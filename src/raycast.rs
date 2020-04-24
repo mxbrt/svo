fa56d4939618b2ab1aaf4e750d44062fe9c15793
Max Breitenfeldt <git@mxbr.me>
2020-04-24 13:08:39 +0200

    Change: Implement SVO-Raycasting non-recursively
    The method is taken from 'Efficient Sparse Voxel Octrees' [1]. It is
    similiar to the previous raycasting method ([2]), but does not use
    a stack. The algorithm was altered to render cubes, by calculating the
    normal for the face that was hit by the ray.
    
    [1] https://users.aalto.fi/~laines9/publications/laine2010i3d_paper.pdf
    [2] An Efficient Parametric Algorithm for Octree Traversal

---
+++ src/raycast.rs
@@ -0,0 +1,11 @@
+use cgmath::{prelude::*, Point3, Vector3};
+
+pub struct RaycastHit {
+    pub color: u32,
+    pub normal: Vector3<f32>,
+    pub pos: Point3<f32>,
+}
+
+pub trait Raycastable {
+    fn raycast(&self, origin: Point3<f32>, dir: Vector3<f32>) -> Option<RaycastHit>;
+}
--- src/svo.rs
+++ src/svo.rs
@@ -1,7 +1,6 @@
-use cgmath::prelude::*;
-use cgmath::{Matrix4, Point3, Vector3};
+use cgmath::{prelude::*, Point3, Vector3};
 
-use crate::render::{RaycastResult, Renderable};
+use crate::raycast::{RaycastHit, Raycastable};
 use crate::voxel_grid::VoxelGrid;
 
 #[derive(Clone, Copy)]
@@ -13,19 +12,18 @@ pub struct Node {
 impl Node {
     fn new() -> Node {
         Node {
-            children: 1 << 31, // default is a terminal node
+            children: 1 << 31, // default is an empty node
             color: 0,
         }
     }
 
-    fn set(&mut self, is_terminal: bool, is_leaf: bool, children: usize, color: u32) {
-        self.children = ((is_terminal as u32) << 31)
-            | ((is_leaf as u32) << 30)
-            | (children as u32 & 0x3fffffff);
+    fn set(&mut self, is_empty: bool, is_leaf: bool, children: usize, color: u32) {
+        self.children =
+            ((is_empty as u32) << 31) | ((is_leaf as u32) << 30) | (children as u32 & 0x3fffffff);
         self.color = color;
     }
 
-    fn is_terminal(&self) -> bool {
+    fn is_empty(&self) -> bool {
         (self.children & 0x80000000) != 0
     }
 
@@ -42,14 +40,6 @@ impl Node {
 pub struct SparseVoxelOctree {
     pub node_pool: Vec<Node>,
     pub size: Vector3<f32>,
-    pub model: Matrix4<f32>,
-}
-
-struct RayData {
-    hit_idx: usize,
-    visit_cnt: u32,
-    a: usize,
-    hit_pos: Point3<f32>,
 }
 
 impl SparseVoxelOctree {
@@ -65,16 +55,17 @@ impl SparseVoxelOctree {
 
         #[rustfmt::skip]
         let (cx,cy, cz) = // Offsets of child cubes
-            ([x, x, x, x, x + half_size, x + half_size, x + half_size, x + half_size],
-             [y, y, y + half_size, y + half_size, y, y, y + half_size, y + half_size],
-             [z, z + half_size, z, z + half_size, z, z + half_size, z, z + half_size]);
+            ([x + half_size, x, x + half_size, x, x + half_size, x, x + half_size, x],
+             [y + half_size, y + half_size, y, y, y + half_size, y + half_size, y, y],
+             [z + half_size, z + half_size, z + half_size, z + half_size, z, z, z, z]);
 
         let node_tile_idx = self.node_pool.len();
         self.node_pool.append(&mut vec![Node::new(); 8]);
 
         for i in 0..8 {
             if voxel_grid.sample(cx[i], cy[i], cz[i], half_size) {
-                let color = (cx[i] | (cy[i] << 8) | (cz[i] << 16)) as u32;
+                //let color = (cx[i] | (cy[i] << 8) | (cz[i] << 16)) as u32;
+                let color = 0xFFFFFF;
                 if half_size != 1 {
                     let child_idx = self.build_octree(voxel_grid, cx[i], cy[i], cz[i], half_size);
                     self.node_pool[node_tile_idx + i].set(false, false, child_idx, color);
@@ -86,209 +77,272 @@ impl SparseVoxelOctree {
         return node_tile_idx;
     }
 
-    fn new_node(&self, txm: f32, x: usize, tym: f32, y: usize, tzm: f32, z: usize) -> usize {
-        if txm < tym {
-            // YZ plane
-            if txm < tzm {
-                return x;
-            }
-        } else {
-            // XZ plane
-            if tym < tzm {
-                return y;
-            }
-        }
-        return z; // XY plane;
-    }
-
-    fn first_node(&self, tx0: f32, ty0: f32, tz0: f32, txm: f32, tym: f32, tzm: f32) -> usize {
-        let mut idx = 0;
-        if tx0 > ty0 {
-            if tx0 > tz0 {
-                if tym < tx0 {
-                    idx |= 2;
-                }
-                if tzm < tx0 {
-                    idx |= 1;
-                }
-                return idx;
-            }
-        } else {
-            if ty0 > tz0 {
-                if txm < ty0 {
-                    idx |= 4;
-                }
-                if tzm < ty0 {
-                    idx |= 1;
-                }
-                return idx;
-            }
-        }
-        if txm < tz0 {
-            idx |= 4;
-        }
-        if tym < tz0 {
-            idx |= 2;
-        }
-        return idx;
-    }
-
-    fn proc_subtree(
+    fn raymarch(
         &self,
-        tx0: f32,
-        ty0: f32,
-        tz0: f32,
-        tx1: f32,
-        ty1: f32,
-        tz1: f32,
-        idx: usize,
-        ray_data: &mut RayData,
+        mut o: Point3<f32>,
+        mut d: Vector3<f32>,
+        t: &mut f32,
+        color: &mut u32,
+        normal: &mut Vector3<f32>,
     ) -> bool {
-        if tx1 < 0.0 || ty1 < 0.0 || tz1 < 0.0 {
-            return false;
+        // Maximum scale (number of float mantissa bits).
+        const S_MAX: u32 = 23;
+        const EPSILON: f32 = 1e-4; // TODO use exp2f(-S_MAX)
+        let mut stack: [(usize, f32); (S_MAX + 1) as usize] = [(0, 0.0); (S_MAX + 1) as usize];
+
+        o.x += 1.0;
+        o.y += 1.0;
+        o.z += 1.0;
+
+        // Get rid of small ray direction components to avoid division by zero.
+        // TODO copysignf
+        if d.x.abs() < EPSILON {
+            d.x = EPSILON
+        }
+        if d.y.abs() < EPSILON {
+            d.y = EPSILON
+        }
+        if d.z.abs() < EPSILON {
+            d.z = EPSILON
         }
 
-        let node = self.node_pool[idx];
-        let children_idx = node.child_idx();
-        ray_data.visit_cnt += 1;
+        // Precompute the coefficients of tx(x), ty(y), and tz(z).
+        let tx_coef: f32 = 1.0 / -d.x.abs();
+        let ty_coef: f32 = 1.0 / -d.y.abs();
+        let tz_coef: f32 = 1.0 / -d.z.abs();
 
-        if node.is_leaf() {
-            ray_data.hit_idx = idx;
-            ray_data.hit_pos.x = tx0;
-            ray_data.hit_pos.y = ty0;
-            ray_data.hit_pos.z = tz0;
-            return true;
+        let mut tx_bias: f32 = tx_coef * o.x;
+        let mut ty_bias: f32 = ty_coef * o.y;
+        let mut tz_bias: f32 = tz_coef * o.z;
+
+        let mut octant_mask: u32 = 7;
+        if d.x > 0.0 {
+            octant_mask ^= 1;
+            tx_bias = 3.0 * tx_coef - tx_bias;
+        }
+        if d.y > 0.0 {
+            octant_mask ^= 2;
+            ty_bias = 3.0 * ty_coef - ty_bias;
+        }
+        if d.z > 0.0 {
+            octant_mask ^= 4;
+            tz_bias = 3.0 * tz_coef - tz_bias;
         }
 
-        if node.is_terminal() {
-            return false;
+        // Initialize the active span of t-values.
+        let mut t_min: f32 = (2.0 * tx_coef - tx_bias)
+            .max(2.0 * ty_coef - ty_bias)
+            .max(2.0 * tz_coef - tz_bias);
+        let mut t_max: f32 = (tx_coef - tx_bias)
+            .min(ty_coef - ty_bias)
+            .min(tz_coef - tz_bias);
+        t_min = t_min.max(0.0);
+
+        let mut parent_idx = 0;
+        let mut cur = 0;
+        let mut cur_node = self.node_pool[0];
+        let mut idx: u32 = 0;
+        let mut pos = Point3::<f32>::new(1.0, 1.0, 1.0);
+        let mut scale: u32 = S_MAX - 1;
+        let mut scale_exp2: f32 = 0.5; // exp2f(scale - s_max)
+        let mut step_mask: u32 = 0;
+
+        if 1.5 * tx_coef - tx_bias > t_min {
+            idx ^= 1;
+            pos.x = 1.5;
+        }
+        if 1.5 * ty_coef - ty_bias > t_min {
+            idx ^= 2;
+            pos.y = 1.5;
+        }
+        if 1.5 * tz_coef - tz_bias > t_min {
+            idx ^= 4;
+            pos.z = 1.5;
         }
 
-        let txm = 0.5 * (tx0 + tx1);
-        let tym = 0.5 * (ty0 + ty1);
-        let tzm = 0.5 * (tz0 + tz1);
+        // Traverse voxels along the ray as long as the current voxel stays within the octree
+        while scale < S_MAX {
+            // Fetch child descriptor unless it is already valid
+            if cur == 0 {
+                cur_node = self.node_pool[parent_idx];
+            }
 
-        let mut cur_node = self.first_node(tx0, ty0, tz0, txm, tym, tzm);
-        let mut is_hit = false;
-        while !is_hit {
-            let next = children_idx + (cur_node ^ ray_data.a);
-            match cur_node {
-                0 => {
-                    is_hit = self.proc_subtree(tx0, ty0, tz0, txm, tym, tzm, next, ray_data);
-                    cur_node = self.new_node(txm, 4, tym, 2, tzm, 1);
-                }
-                1 => {
-                    is_hit = self.proc_subtree(tx0, ty0, tzm, txm, tym, tz1, next, ray_data);
-                    cur_node = self.new_node(txm, 5, tym, 3, tz1, 8);
-                }
-                2 => {
-                    is_hit = self.proc_subtree(tx0, tym, tz0, txm, ty1, tzm, next, ray_data);
-                    cur_node = self.new_node(txm, 6, ty1, 8, tzm, 3);
-                }
-                3 => {
-                    is_hit = self.proc_subtree(tx0, tym, tzm, txm, ty1, tz1, next, ray_data);
-                    cur_node = self.new_node(txm, 7, ty1, 8, tz1, 8);
-                }
-                4 => {
-                    is_hit = self.proc_subtree(txm, ty0, tz0, tx1, tym, tzm, next, ray_data);
-                    cur_node = self.new_node(tx1, 8, tym, 6, tzm, 5);
-                }
-                5 => {
-                    is_hit = self.proc_subtree(txm, ty0, tzm, tx1, tym, tz1, next, ray_data);
-                    cur_node = self.new_node(tx1, 8, tym, 7, tz1, 8);
-                }
-                6 => {
-                    is_hit = self.proc_subtree(txm, tym, tz0, tx1, ty1, tzm, next, ray_data);
-                    cur_node = self.new_node(tx1, 8, ty1, 8, tzm, 7);
-                }
-                7 => {
-                    is_hit = self.proc_subtree(txm, tym, tzm, tx1, ty1, tz1, next, ray_data);
-                    cur_node = 8;
-                }
-                _ => {
-                    return false;
+            // Determine maximum t-value of the cube by evaluating
+            // tx(), ty(), and tz() at its corner.
+            let tx_corner: f32 = pos.x * tx_coef - tx_bias;
+            let ty_corner: f32 = pos.y * ty_coef - ty_bias;
+            let tz_corner: f32 = pos.z * tz_coef - tz_bias;
+            let tc_max = tx_corner.min(ty_corner).min(tz_corner);
+
+            // Process voxel if it exists and the active t-span is non-empty.
+            let child_idx = cur_node.child_idx() + (idx ^ octant_mask) as usize;
+            let child = self.node_pool[child_idx];
+
+            if !child.is_empty() && t_min <= t_max {
+                //// TODO Terminate if the voxel is small enough.
+                //if tc_max * ray_size_coef + ray_size_bias >= scale_exp2 {
+                //break;
+                //}
+
+                // INTERSECT
+                // Intersect active t-span with the cube and evaluate
+                // tx(), ty(), and tz() at the center of the voxel.
+                let tv_max: f32 = t_max.min(tc_max);
+                let half: f32 = scale_exp2 * 0.5;
+                let tx_center: f32 = half * tx_coef + tx_corner;
+                let ty_center: f32 = half * ty_coef + ty_corner;
+                let tz_center: f32 = half * tz_coef + tz_corner;
+
+                // Descend to the first child if the resulting t-span is non-empty.
+                if t_min <= tv_max {
+                    if child.is_leaf() {
+                        cur_node = child;
+                        break;
+                    }
+
+                    // PUSH
+                    // Write current parent to the stack
+                    stack[scale as usize] = (parent_idx, t_max);
+                    parent_idx = child_idx;
+                    idx = 0;
+                    scale -= 1;
+                    scale_exp2 = half;
+
+                    if tx_center > t_min {
+                        idx ^= 1;
+                        pos.x += scale_exp2;
+                    }
+                    if ty_center > t_min {
+                        idx ^= 2;
+                        pos.y += scale_exp2;
+                    }
+                    if tz_center > t_min {
+                        idx ^= 4;
+                        pos.z += scale_exp2;
+                    }
+                    // Update active t-span and invalidate cached child descriptor.
+                    t_max = tv_max;
+                    cur = 0;
+                    continue;
                 }
             }
+            // ADVANCE
+            // Step along the ray
+            step_mask = 0;
+            if tx_corner <= tc_max {
+                step_mask ^= 1;
+                pos.x -= scale_exp2;
+            }
+            if ty_corner <= tc_max {
+                step_mask ^= 2;
+                pos.y -= scale_exp2;
+            }
+            if tz_corner <= tc_max {
+                step_mask ^= 4;
+                pos.z -= scale_exp2;
+            }
+
+            // Update active t-span and flip bits of the child slot index.
+            t_min = tc_max;
+            idx ^= step_mask;
+
+            // Proceed with pop if the bit flips disagree with the ray direction.
+            if (idx & step_mask) != 0 {
+                // POP
+                // Find the highest differing bit between the two positions.
+                let mut differing_bits: u32 = 0;
+                if (step_mask & 1) != 0 {
+                    differing_bits |= pos.x.to_bits() ^ (pos.x + scale_exp2).to_bits();
+                }
+                if (step_mask & 2) != 0 {
+                    differing_bits |= pos.y.to_bits() ^ (pos.y + scale_exp2).to_bits();
+                }
+                if (step_mask & 4) != 0 {
+                    differing_bits |= pos.z.to_bits() ^ (pos.z + scale_exp2).to_bits();
+                }
+                scale = 31 - (differing_bits).leading_zeros(); // position of the highest bit
+                scale_exp2 = f32::from_bits(((scale as i32 - S_MAX as i32 + 127) << 23) as u32); // exp2f(scale - s_max)
+
+                // Restore parent voxel from the stack.
+                parent_idx = stack[scale as usize].0;
+                t_max = stack[scale as usize].1;
+
+                // Round cube position and extract child slot index.
+                let shx: u32 = pos.x.to_bits() >> scale;
+                let shy: u32 = pos.y.to_bits() >> scale;
+                let shz: u32 = pos.z.to_bits() >> scale;
+                pos.x = f32::from_bits(shx << scale);
+                pos.y = f32::from_bits(shy << scale);
+                pos.z = f32::from_bits(shz << scale);
+                idx = (shx & 1) | ((shy & 1) << 1) | ((shz & 1) << 2);
+                cur = 0;
+            }
         }
+
+        if scale >= S_MAX {
+            return false;
+        }
+
+        // this happens for boundary voxels
+        if step_mask == 0 {
+            if 2.0 * tx_coef - tx_bias >= t_min {
+                step_mask ^= 1;
+            }
+            if 2.0 * ty_coef - ty_bias >= t_min {
+                step_mask ^= 2;
+            }
+            if 2.0 * tz_coef - tz_bias >= t_min {
+                step_mask ^= 4;
+            }
+        }
+
+        let face;
+        if (octant_mask & 1) == 0 && (step_mask & 1) != 0 {
+            face = 3;
+        } else if (octant_mask & 2) == 0 && (step_mask & 2) != 0 {
+            face = 5;
+        } else if (octant_mask & 4) == 0 && (step_mask & 4) != 0 {
+            face = 6;
+        } else {
+            face = step_mask;
+        }
+
+        match face {
+            // right
+            1 => normal.x = 1.0,
+            // top
+            2 => normal.y = 1.0,
+            // left
+            3 => normal.x = -1.0,
+            // back
+            4 => normal.z = 1.0,
+            // bottom
+            5 => normal.y = -1.0,
+            // front
+            6 => normal.z = -1.0,
+            _ => (),
+        }
+
+        *t = t_min;
+        *color = cur_node.color;
         return true;
     }
 }
 
-impl Renderable for SparseVoxelOctree {
-    fn raycast(&self, origin: Point3<f32>, dir: Vector3<f32>, result: &mut RaycastResult) -> bool {
-        let mut ray_data = RayData {
-            hit_idx: 0,
-            visit_cnt: 0,
-            a: 0,
-            hit_pos: Point3::origin(),
-        };
-        let mut origin = self.model.transform_point(origin);
-        let mut dir = self.model.transform_vector(dir);
-        if dir.x < 0.0 {
-            origin.x = self.size.x - origin.x;
-            dir.x = -dir.x;
-            ray_data.a |= 4;
-        }
-        if dir.y < 0.0 {
-            origin.y = self.size.y - origin.y;
-            dir.y = -dir.y;
-            ray_data.a |= 2;
-        }
-        if dir.z < 0.0 {
-            origin.z = self.size.z - origin.z;
-            dir.z = -dir.z;
-            ray_data.a |= 1;
-        }
-
-        let tx0 = -origin.x / dir.x;
-        let ty0 = -origin.y / dir.y;
-        let tz0 = -origin.z / dir.z;
-        let tx1 = (self.size.x - origin.x) / dir.x;
-        let ty1 = (self.size.y - origin.y) / dir.y;
-        let tz1 = (self.size.z - origin.z) / dir.z;
-
-        if f32::max(f32::max(tx0, ty0), tz0) < f32::min(f32::min(tx1, ty1), tz1) {
-            if self.proc_subtree(tx0, ty0, tz0, tx1, ty1, tz1, 0, &mut ray_data) {
-                result.visit_cnt = ray_data.visit_cnt;
-                result.color = self.node_pool[ray_data.hit_idx].color;
-                let tx0 = ray_data.hit_pos.x;
-                let ty0 = ray_data.hit_pos.y;
-                let tz0 = ray_data.hit_pos.z;
-                let face;
-                if tx0 > ty0 && tx0 > tz0 {
-                    face = 0 | (ray_data.a & 4);
-                } else if ty0 > tx0 && ty0 > tz0 {
-                    face = 1 | (ray_data.a & 2);
-                } else {
-                    face = 2 | ((ray_data.a & 1) << 2);
-                }
-                match face {
-                    // left
-                    0 => result.normal.x = -1.0,
-                    // bottom
-                    1 => result.normal.y = -1.0,
-                    // front
-                    2 => result.normal.z = 1.0,
-                    // top
-                    3 => result.normal.y = 1.0,
-                    // right side
-                    4 => result.normal.x = 1.0,
-                    // back
-                    6 => result.normal.z = -1.0,
-                    _ => println!("Invalid value for cube face."),
-                }
-                result.normal = self.model.transform_vector(result.normal);
-                return true;
-            } else {
-                return false;
-            }
+impl Raycastable for SparseVoxelOctree {
+    fn raycast(&self, origin: Point3<f32>, dir: Vector3<f32>) -> Option<RaycastHit> {
+        let mut color = 0;
+        let mut normal = Vector3::zero();
+        let mut t = 0.0;
+        if self.raymarch(origin, dir, &mut t, &mut color, &mut normal) {
+            return Some(RaycastHit {
+                color: color,
+                normal: normal,
+                pos: origin + t * dir,
+            });
         } else {
-            return false;
+            return None;
         }
-    }
-
-    fn get_model(&self) -> &Matrix4<f32> {
-        return &self.model;
     }
 }
 
@@ -297,10 +351,9 @@ impl From<&VoxelGrid> for SparseVoxelOctree {
         let mut svo = SparseVoxelOctree {
             node_pool: Vec::<Node>::new(),
             size: Vector3::new(1.0, 1.0, 1.0),
-            model: Matrix4::from_translation(Vector3::zero()),
         };
         svo.node_pool.push(Node::new());
-        svo.node_pool[0].set(false, false, 1, 0);
+        svo.node_pool[0].set(false, false, 1, 0xFF00FF);
         svo.build_octree(&voxel_grid, 0, 0, 0, voxel_grid.size);
         return svo;
     }
